fdf43c5e6bc07e3c24959ee7c185eebbcd7b68c2
liter <liter@daviddemac-mini.local>
2016-10-20 09:20:16 +0800

    Initial Commit

---
+++ 学习OpenGL1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  学习OpenGL1
+//
+//  Created by liter on 16/10/20.
+//  Copyright © 2016年 liter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
